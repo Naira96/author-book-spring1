e5ca578b091e50607b5c8adc745fc895d9bb8fa3
Naira <akademia.96@mail.ru>
2021-03-02 19:59:32 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApApplication.class, args);
+    }
+
+}
